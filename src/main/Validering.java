65a9dc69b06d1dc5ff5ca776729dc50b280f06f3
Marius Irgens <mariusirgens@mail.com>
2019-11-01 14:00:18 +0100

    Update Validering.java

--- src/main/Validering.java
+++ src/main/Validering.java
@@ -1,24 +1,18 @@
 package main;
 
-import java.util.regex.Pattern;
-
 // Klasse som validerer bruker input
-public class Validering {
+public class validering {
 
-	public static Boolean ValiderNavn(String navn) {
-		return (navn.length() > 2 && navn.length() < 20) && Pattern.matches("[A-ZÆØÅ]", navn.substring(0, 0)) && Pattern.matches("[a-zA-Z æøåÆØÅ]", navn);
+	public static boolean ValiderNavn(String navn) {
+		return (navn.length() > 2 && navn.length() < 20) && navn.matches("[a-zA-Z Ã¦Ã¸Ã¥Ã†Ã˜Ã…]+");
 	}
 
-	public static Boolean ValiderMobil(String mobil) {
-		return (mobil.length() == 8 && Pattern.matches("[0-9]", mobil));
+	public static boolean ValiderMobil(String mobil) {
+		return (mobil.length() == 8 && mobil.matches("[0-9]+"));
 	}
 
-	public static Boolean ValiderKjonn(String kjonn) {
-		return (kjonn == "mann" || kjonn == "kvinne");
-	}
-	
-	public static Boolean ValiderPassordRep(String pass, String rep) {
-		return pass.equals(rep);
+	public static boolean ValiderKjonn(String kjonn) {
+		return (kjonn.equals("mann") || kjonn.equals("kvinne"));
 	}
 
 	/*(?=.*[0-9]) a digit must occur at least once
@@ -28,28 +22,32 @@ public class Validering {
 (?=\\S+$) no whitespace allowed in the entire string
 .{8,} at least 8 characters
 */
-	public static Boolean ValiderPassord(String pass) 
+	
+	public static void main(String[] args) {
+		System.out.println(ValiderPassord("1234ABcd&"));
+	}
+	public static boolean ValiderPassord(String pass) 
 	{
-		String passwordcheck = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\\\S+$).{8,}";
+		String passwordcheck = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=.\\s+$).{8,}";
 		
-		return Pattern.matches(passwordcheck, pass) && pass.length() <20;
+		return pass.matches(passwordcheck) && pass.length() < 20;
+	}
+	
+	public static Boolean ValiderPassordRep(String pass, String rep) {
+		return pass.equals(rep);
 	}
 
-	public static boolean gyldigDeltager(String fornavn, String etternavn, String mobil, String passord, String passordRep, String kjonn) {
-		return (
-				ValiderNavn(fornavn) && 
+	public static boolean gyldigDeltager(String fornavn, String etternavn, String mobil, String passord, String kjonn) {
+		return (				
+				ValiderNavn(fornavn) &&
 				ValiderNavn(etternavn) &&
 				ValiderMobil(mobil) &&
-				ValiderKjonn(kjonn) && 
-				ValiderPassord(passord)&&
-				ValiderPassordRep(passord, passordRep)
+				ValiderKjonn(kjonn)
+				//ValiderPassord(passord) 
 				);
 	}
 	
 	public static String ForceForbokstav(String input) {
-		if(input.length()== 0) {
-			return input;
-		}
 		String forbokstav = input.substring(0, 1);
 		forbokstav = forbokstav.toUpperCase();
 		
